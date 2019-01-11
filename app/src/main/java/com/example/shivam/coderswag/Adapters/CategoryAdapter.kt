310bc13ad3044367d34615916afc65b3059f3097
unknown <shivamk522@gmail.com>
2019-01-11 12:34:20 +0530

    customeAdapter injected to the ListView

---
+++ app/src/main/java/com/example/shivam/coderswag/Adapters/CategoryAdapter.kt
@@ -0,0 +1,69 @@
+package com.example.shivam.coderswag.Adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import com.example.shivam.coderswag.Model.Category
+import com.example.shivam.coderswag.R
+
+class CategoryAdapter(context: Context, categories: List<Category>) : BaseAdapter() {
+
+    val context = context
+    val categories = categories
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        val categoryView: View
+        val holder : ViewHolder
+
+        if (convertView == null)
+        {
+            categoryView = LayoutInflater.from(context).inflate(R.layout.category_list_item, null)
+            holder = ViewHolder()
+
+            holder.categoryImage = categoryView.findViewById(R.id.categoryimage)
+            holder.categoryName = categoryView.findViewById(R.id.categoryName)
+
+            categoryView.tag = holder
+        }
+        else
+        {
+            holder = convertView.tag as ViewHolder
+            categoryView = convertView
+        }
+
+
+        val category = categories[position]
+        val resourceId = context.resources.getIdentifier(category.image, "drawable", context.packageName)
+
+        holder.categoryImage?.setImageResource(resourceId)
+        holder.categoryName?.text = category.title
+
+        return categoryView
+
+    }
+
+    override fun getItem(position: Int): Any {
+        return categories[position]
+    }
+
+    override fun getItemId(position: Int): Long {
+        return 0
+    }
+
+    override fun getCount(): Int {
+        return categories.count()
+    }
+
+
+    private class ViewHolder
+    {
+        var categoryImage: ImageView? = null
+        var categoryName : TextView? = null
+    }
+
+
+}
--- app/src/main/java/com/example/shivam/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/example/shivam/coderswag/Controller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.shivam.coderswag.Controller
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import com.example.shivam.coderswag.Adapters.CategoryAdapter
 import com.example.shivam.coderswag.Model.Category
 import com.example.shivam.coderswag.R
 import com.example.shivam.coderswag.Services.DataService
@@ -10,15 +11,13 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var adapter : ArrayAdapter<Category>
+    lateinit var adapter : CategoryAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        adapter = ArrayAdapter(this,
-            android.R.layout.simple_list_item_1,
-            DataService.categories)
+        adapter = CategoryAdapter(this, DataService.categories)
 
         categoryListView.adapter = adapter
     }
