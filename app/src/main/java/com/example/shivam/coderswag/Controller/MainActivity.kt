db1025fabcbc0c7a873d1d8ba8e5222c80fee4eb
unknown <shivamk522@gmail.com>
2019-01-11 14:02:06 +0530

    Toast Added

--- app/src/main/java/com/example/shivam/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/example/shivam/coderswag/Controller/MainActivity.kt
@@ -2,7 +2,10 @@ package com.example.shivam.coderswag.Controller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.Toast
 import com.example.shivam.coderswag.Adapters.CategoryAdapter
 import com.example.shivam.coderswag.Model.Category
 import com.example.shivam.coderswag.R
@@ -11,14 +14,20 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var adapter : CategoryAdapter
+    lateinit var adapter: CategoryAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         adapter = CategoryAdapter(this, DataService.categories)
-
         categoryListView.adapter = adapter
+
+        categoryListView.setOnItemClickListener { parent, view, position, id ->
+            val category = DataService.categories[position]
+            Toast.makeText(this, "You clicked on the ${category.title} cell", Toast.LENGTH_SHORT).show()
+        }
+
+
     }
 }
